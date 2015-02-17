8f0fdc5ea719cbde82065e2fb6aa970c49b2a387
Andrei Avram <andrei.avram@gmail.com>
2015-02-17 06:22:39 +0200

    small updated, functioning example

--- django_multivent/urls.py
+++ django_multivent/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from multivent.views import TestPlanner
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,4 +8,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    (r'svg/test/$', TestPlanner.as_view(), {}, "test_planner"),
 )
